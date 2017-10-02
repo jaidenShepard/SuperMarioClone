547795584557086637831123831d32b6fc248232
Jayden Shepard <jaydenshepard@gmail.com>
2017-10-02 10:07:44 -0400

    More sprites and finished ground layout

---
+++ Mario Clone/Assets/Scripts/Camera_Scroll.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class Camera_Scroll : MonoBehaviour {
+
+    public GameObject mario;
+    public float leftbound;
+    public float rightbound;
+
+    private Vector3 offset;
+
+	// Use this for initialization
+	void Start () {
+        offset = transform.position - mario.transform.position;
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+        transform.position = mario.transform.position + offset;
+        Mathf.Clamp(transform.position.x, leftbound, rightbound);
+	}
+}
---
+++ Mario Clone/Assets/Scripts/Mario_Controls.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class Mario_Controls : MonoBehaviour {
+    public float speed;
+
+    private Rigidbody2D rb;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody2D>();	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, 0.0f);
+
+        transform.position += movement * speed * Time.deltaTime;
+    }
+}
